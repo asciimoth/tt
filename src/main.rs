df6843034b8e2f370edc8915086cd8be80ad1ba9
John Doe <>
2022-03-15 16:43:39 +0300

    Add physics

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ struct Space<T>{
     h: usize,
 }
 
-type Mask = Space<()>;
+pub type Mask = Space<()>;
 
 impl<T> Space<T>{
     pub fn new(w: usize, h: usize) -> Self{
@@ -57,6 +57,23 @@ impl<T> Space<T>{
             yc += 1;
         }
         Ok(())
+    }
+    pub fn copy_in_with_bounds(&mut self, mut x: usize, mut y: usize, other: Space<T>) {
+        if x+other.w > self.w{
+            x -= (x+other.w)-self.w
+        }
+        if y+other.h> self.h{
+            y -= (y+other.h)-self.h
+        }
+        let mut yc = 0;
+        for raw in other.space {
+            let mut xc = 0;
+            for v in raw {
+                self.space[yc+y][xc+x] = v;
+                xc += 1;
+            }
+            yc += 1;
+        }
     }
     pub fn copy_in_with_mask(&mut self,  x: usize, y: usize, other: Space<T>, mask: Mask) -> Result<(), &'static str> {
         if other.w != mask.w {
@@ -241,125 +258,153 @@ impl fmt::Debug for Colors {
     }
 }
 
-#[derive(Debug, Copy, Clone, PartialEq)]
-pub enum ShapeType{
-    I,
-    O,
-    L,
-    J,
-    S,
-    Z,
-    T,
-}
+pub type Desk = Space<Colors>;
 
-impl ShapeType{
-    pub fn get_random<R: Rng + ?Sized>(rng: &mut R) -> ShapeType{
-        let rand = rng.gen_range(0..7);
-        match rand{
-            0 => ShapeType::I,
-            1 => ShapeType::O,
-            2 => ShapeType::L,
-            3 => ShapeType::J,
-            4 => ShapeType::S,
-            5 => ShapeType::Z,
-            _ => ShapeType::T,
-        }
-    }
-}
-
-pub struct Shape{
-    shape: Space<Colors>,
-    mask: Mask,
-}
-
-impl Shape{
-    pub fn new(tp: ShapeType, color: Colors) -> Self{
-        let mut shape = Space::<Colors>::new(5,5);
-        match tp {
-            ShapeType::I => {
-                shape.set_no_err(2,1, Some(color));
-                shape.set_no_err(2,2, Some(color));
-                shape.set_no_err(2,3, Some(color));
-                shape.set_no_err(2,4, Some(color));
-            }
-            ShapeType::O => {
-                shape.set_no_err(2,2, Some(color));
-                shape.set_no_err(2,3, Some(color));
-                shape.set_no_err(3,2, Some(color));
-                shape.set_no_err(3,3, Some(color));
-            }
-            ShapeType::L => {
-                shape.set_no_err(2,1, Some(color));
-                shape.set_no_err(2,2, Some(color));
-                shape.set_no_err(2,3, Some(color));
-                shape.set_no_err(3,3, Some(color));
-            }
-            ShapeType::J => {
-                shape.set_no_err(2,1, Some(color));
-                shape.set_no_err(2,2, Some(color));
-                shape.set_no_err(2,3, Some(color));
-                shape.set_no_err(1,3, Some(color));
-            }
-            ShapeType::S => {
-                shape.set_no_err(2,1, Some(color));
-                shape.set_no_err(3,1, Some(color));
-                shape.set_no_err(1,2, Some(color));
-                shape.set_no_err(2,2, Some(color));
-            }
-            ShapeType::Z => {
-                shape.set_no_err(1,1, Some(color));
-                shape.set_no_err(2,1, Some(color));
-                shape.set_no_err(2,2, Some(color));
-                shape.set_no_err(3,2, Some(color));
-            }
-            ShapeType::T => {
-                shape.set_no_err(2,2, Some(color));
-                shape.set_no_err(1,3, Some(color));
-                shape.set_no_err(2,3, Some(color));
-                shape.set_no_err(3,3, Some(color));
-            }
-        }
-        let mask = Mask::from_space(&shape);
-        Shape{shape, mask}
-    }
-}
-
-impl fmt::Debug for Shape {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let mut minx: usize = self.mask.w;
-        let mut maxx: usize = 0;
-        let mut miny: usize = self.mask.h;
-        let mut maxy: usize = 0;
-        for x in 0..self.mask.w {
-            for y in 0..self.mask.h {
-                if let Some(()) = self.mask.get_no_err(x,y){
-                    if x > maxx { maxx = x; }
-                    if x < minx { minx = x; }
-                    if y > maxy { maxy = y; }
-                    if y < miny { miny = y; }
+impl Desk {
+    pub fn fall(&mut self) -> bool {
+        let mut ret = false;
+        for y in (0..self.h-1).rev() {
+            for x in 0..self.w {
+                if let Some(_) = self.space[y][x]{
+                    if let None = self.space[y+1][x]{
+                        self.space[y+1][x] = self.space[y][x];
+                        self.space[y][x] = None;
+                        ret = true;
+                    }
                 }
             }
         }
-        for y in miny..maxy+1 {
-            for x in minx..maxx+1 {
-                if let Some(color) = self.shape.space[y][x] {
-                    write!(f, "{:?}", color)?;
-                }else{
-                    write!(f, " ")?;
+        ret
+    }
+    pub fn remove(&mut self) -> bool {
+        let mut ret = false;
+        for y in 0..self.h{
+            let mut rm = true;
+            for x in 0..self.w{
+                if let None = self.space[y][x]{
+                    rm = false;
+                    break;
                 }
             }
-            write!(f, "\n")?;
+            if rm {
+                ret = true;
+                for x in 0..self.w{
+                    self.space[y][x] = None;
+                }
+            }
         }
-        Ok(())
+        ret
+    }
+    pub fn step(&mut self) -> bool {
+        if self.fall() { return true }
+        if self.remove() { return true }
+        false
+    }
+    pub fn get_content_heigth(&self) -> usize {
+        for y in 0..self.h {
+            for x in 0..self.w {
+                if let Some(_) = self.space[y][x]{
+                    return self.h-y
+                }
+            }
+        }
+        0
+    }
+}
+
+fn get_i_shape(color: Colors) -> Desk {
+    let color = Some(color);
+    let mut shape = Desk::new(1,4);
+    shape.set_no_err(0, 0, color);
+    shape.set_no_err(0, 1, color);
+    shape.set_no_err(0, 2, color);
+    shape.set_no_err(0, 3, color);
+    shape
+}
+
+fn get_o_shape(color: Colors) -> Desk {
+    let color = Some(color);
+    let mut shape = Desk::new(2,2);
+    shape.set_no_err(0, 0, color);
+    shape.set_no_err(0, 1, color);
+    shape.set_no_err(1, 0, color);
+    shape.set_no_err(1, 1, color);
+    shape
+}
+
+fn get_l_shape(color: Colors) -> Desk {
+    let color = Some(color);
+    let mut shape = Desk::new(2,3);
+    shape.set_no_err(0, 0, color);
+    shape.set_no_err(0, 1, color);
+    shape.set_no_err(0, 2, color);
+    shape.set_no_err(1, 2, color);
+    shape
+}
+
+fn get_j_shape(color: Colors) -> Desk {
+    let color = Some(color);
+    let mut shape = Desk::new(2,3);
+    shape.set_no_err(1, 0, color);
+    shape.set_no_err(1, 1, color);
+    shape.set_no_err(1, 2, color);
+    shape.set_no_err(0, 2, color);
+    shape
+}
+
+fn get_s_shape(color: Colors) -> Desk {
+    let color = Some(color);
+    let mut shape = Desk::new(3,2);
+    shape.set_no_err(0, 1, color);
+    shape.set_no_err(1, 1, color);
+    shape.set_no_err(1, 0, color);
+    shape.set_no_err(2, 0, color);
+    shape
+}
+
+fn get_z_shape(color: Colors) -> Desk {
+    let color = Some(color);
+    let mut shape = Desk::new(3,2);
+    shape.set_no_err(2, 1, color);
+    shape.set_no_err(1, 1, color);
+    shape.set_no_err(1, 0, color);
+    shape.set_no_err(0, 0, color);
+    shape
+}
+
+fn get_t_shape(color: Colors) -> Desk {
+    let color = Some(color);
+    let mut shape = Desk::new(3,2);
+    shape.set_no_err(2, 1, color);
+    shape.set_no_err(1, 1, color);
+    shape.set_no_err(1, 0, color);
+    shape.set_no_err(0, 1, color);
+    shape
+}
+
+fn get_random_shape<R: Rng + ?Sized>(rng: &mut R, color: Colors) -> Desk {
+    match rng.gen_range(0..7) {
+        0 => { get_i_shape(color) }
+        1 => { get_o_shape(color) }
+        2 => { get_l_shape(color) }
+        3 => { get_j_shape(color) }
+        4 => { get_s_shape(color) }
+        5 => { get_z_shape(color) }
+        _ => { get_t_shape(color) }
     }
 }
 
 fn main() {
-    let mut rng = StdRng::seed_from_u64(1);
-    for _ in 0..10 {
-        let shape_type = ShapeType::get_random(&mut rng);
-        let color = Colors::get_random(&mut rng);
-        let shape = Shape::new(shape_type, color);
-        println!("{:?}", shape);
+    let mut rng = StdRng::seed_from_u64(5);
+    let color = Colors::get_random(&mut rng);
+    let ax = rng.gen_range(0..4) ;
+    let shape = get_random_shape(&mut rng, color).get_rotated(Rotation::Clockwise, ax);
+    let mut desk = Desk::new(10,20);
+    desk.copy_in_with_bounds(3,3,shape);
+    let mut shape = Desk::new_default(10,2,Some(color));
+    desk.copy_in_with_bounds(0,10,shape);
+    println!("{:?}", desk);
+    while desk.step() {
+        println!("{:?}\n{:?}", desk.get_content_heigth(), desk);
     }
 }
