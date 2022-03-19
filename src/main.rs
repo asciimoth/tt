e126acf0e42baf57f1b1a179b10f2039f808f680
DomesticMoth <91414737+DomesticMoth@users.noreply.github.com>
2022-03-19 16:51:43 +0300

    Add licence header

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+// tt by DomesticMoth
+// 
+// To the extent possible under law, the person who associated CC0 with
+// tt has waived all copyright and related or neighboring rights
+// to tt.
+// 
+// You should have received a copy of the CC0 legalcode along with this
+// work.  If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 use std::fmt;
 use std::cmp::min;
 use rand::Rng;
